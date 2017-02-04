8c1177c6a8f3e37942bc44c2fee2dcfda713466e
Bryan Stephens <bryan.stephens@infusionsoft.com>
2017-02-04 14:44:35 -0700

    Adding feature-b, useless difference method

--- src/test/java/com/queensburg/MainTest.java
+++ src/test/java/com/queensburg/MainTest.java
@@ -10,4 +10,10 @@ public class MainTest {
     int sum = Main.someMethod(3, 6);
     assertEquals("someMethod should sum the arguments", 9, sum);
   }
+
+  @Test
+  public void anotherMethodFindsTheDifference() {
+    int difference = Main.anotherMethod(8, 3);
+    assertEquals("anotherMethod should return the difference of the arguments", 5, difference);
+  }
 }
